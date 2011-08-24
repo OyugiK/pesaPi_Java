bad0850f2586c46848cd1b83c6004cecb166325d
OyugiK <Oyugi.K.Owino@gmail.com>
2011-08-24 23:36:04 +0300

    fixed the array and made changes to the errors that were occurring during run-time

--- pesaPi_Java/src/java/com/pesaPi/Utils/configuration.java
+++ pesaPi_Java/src/java/com/pesaPi/Utils/configuration.java
@@ -1,9 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 package com.pesaPi.Utils;
 
 /**
@@ -11,59 +5,69 @@ package com.pesaPi.Utils;
  * @author oyugik
  */
 public class configuration{
-    public static void main (String [] args){
-        String $singleton = "null";
 
-    }
-    protected static (String [] args){
-            // Enable this feature when in production - in order to disable debuginformation
-		String ProductionMode = "false";
+    String [] singleton = {"null"};
+
+
+    class configArray {
+        public void main (String [] args){
+        
+        }
+
+
+
+        // Enable this feature when in production - in order to disable debuginformation
+		String [] ProductionMode = {"false"};
 
 		// Enable this feature when you want to run the API against the simulator
 		// The simulator does not use SSL and is more easy to get up and running.
-		String SimulationMode =  "true";
+		String [] SimulationMode =  {"true"};
 
 		// Enabling this will allow the system to automatically
-		// update the scrubbing methods in use.
+		// update the scrubbing methodsn use.
 		// Hereby ensuring the system will keep running with
 		// minimum downtime, in case of Safaricom changing any code.
-		String $AllowAutoUpdate = "true";
+		String [] AllowAutoUpdate = {"true"};
 
 		// Enabling this feature allows the system to give feedback regarding
     // errors, problems and performance.
 		// feedback to the developers of Mpesapi - hereby making it
 		// possible to better analyse how to improve the system further.
-		String $AllowFeedback = "true";
+		String [] AllowFeedback = {"true"};
 
 		// To ensure the system is a robust as possible you want
 		// to keep this feature active - By doing so you enable
 		// method triangulation to ensure it fallsback to a different
 		// method in case one fails.
 		// the downside is slower performance.
-		String MaxCompatibility = "true";
+		String [] MaxCompatibility = {"true"};
 
 		// Mpesa information
-		String MpesaCertificatePath     = "change_this_path.pem";
-		String MpesaLoginName           = "test";
-		String MpesaPassword            = "best";
-		String MpesaCorporation         = "PesaPi";
-                String MpesaInitialSyncDate     = "2011-01-01";
-		String CookieFolderPath         = ".";
+		String [] MpesaCertificatePath     = {"change_this_path.pem"};
+		String [] MpesaLoginName           = {"test"};
+		String [] MpesaPassword            = {"best"};
+		String [] MpesaCorporation         = {"PesaPi"};
+                String [] MpesaInitialSyncDate     = {"2011-01-01"};
+		String [] CookieFolderPath         = {"."};
 
 		// Database settings follow - please note that they are repeated twice
-		String DatabaseHostRead        = "localhost";
-		String DatabaseUserRead	= "root";
-		String DatabasePasswordRed	= "root";
-                String DatabaseDatabaseRead	= "pesaPi";
-		String DatabaseHostWrite	= "localhost";
-		String DatabaseUserWrite	= "root";
-		"DatabasePasswordWrite"				=> "root",
-		"DatabaseDatabaseWrite"				=> "pesaPi",
+		String [] DatabaseHostRead        = {"localhost"};
+		String [] DatabaseUserRead	= {"root"};
+		String [] DatabasePasswordRed	= {"root"};
+                String [] DatabaseDatabaseRead	= {"pesaPi"};
+		String [] DatabaseHostWrite	= {"localhost"};
+		String [] DatabaseUserWrite	= {"root"};
+		String [] DatabasePasswordWrite				= {"root"};
+		String [] DatabaseDatabaseWrite				= {"pesaPi"};
 
- 		"Version"											=> "0.0.2",
-		"ConfigEnd"                   => ""
-	);
+ 		String [] Version					= {"0.0.2"};
+		String [] ConfigEnd                   = {""};
+	
 
     }
-
+static public void getConfig (String argument) {
+    /*
+     * Dont understand this will come to it later
+     */
+}
 }
